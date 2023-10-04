9debb8e53fb1e7a068fb9a023d781ec832de56c3
AngeloMenendez <angelo.menendez04@gmail.com>
2023-10-04 15:24:16 -0500

    build.sh test

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+DJANGO_SUPERUSER_PASSWORD=emelec14 python manage.py createsuperuser --no-input --username=angelo  --email=angelo.menendez04@gmail.com
